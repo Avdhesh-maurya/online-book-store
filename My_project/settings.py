33c768d333df40773a195f2ca4d2a34eeaef1734
Avdhesh Kumar <akah8887@gmail.com>
2026-01-04 23:05:20 +0530

    Integrate WhiteNoise middleware and static file settings
    Added WhiteNoise middleware to enhance static file handling and configured static file settings.

--- My_project/settings.py
+++ My_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -114,7 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
@@ -126,3 +129,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STRIPE_SECRET_KEY = "sk_test_51ROdy5EAMY5l3s7ngmVCUgChQdryMjzKxYYcnz6PAPp5FVug0LTpRn90xjE1riixMOv1lrHIpBOmImeErmMg7mx400S3ne1amz"
 STRIPE_PUBLISHABLE_KEY = "your_publishable_key"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
