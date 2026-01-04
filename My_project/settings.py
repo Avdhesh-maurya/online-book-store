0428c5bb37c4e1501dc33e6266657c9f1f19d49e
Avdhesh Kumar <akah8887@gmail.com>
2026-01-04 21:55:01 +0530

    Set DEBUG to False and allow all hosts
    Changed DEBUG setting to False and updated ALLOWED_HOSTS.

--- My_project/settings.py
+++ My_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-@3asm_nvo7wyose7372er69!15gn+lrezc4w84_u=gy^zoe62x"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
